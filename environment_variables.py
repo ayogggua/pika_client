842092652c6869a5e8d25b50213aca8c35e15ef2
Jayakrishnan Damodaran <jayakrishnandamodaran@gmail.com>
2019-06-23 13:33:11 +0530

    Added a publish interval variable in environment properly.

--- environment_variables.py
+++ environment_variables.py
@@ -3,4 +3,4 @@ import os
 
 class EnvironmentVariable(object):
     AMQP_URL = os.environ.get('AMQP_URL')
-    PUBLISH_INTERVAL = 5  # seconds.
+    PUBLISH_INTERVAL = os.environ.get('PUBLISH_INTERVAL', 5)  # seconds.
